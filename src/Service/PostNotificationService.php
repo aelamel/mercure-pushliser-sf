36cb01b2ff68e12d2cb9be6583c9065e54a9f78d
Abdelkarim EL AMEL <aelamel@octo.com>
2020-01-24 22:11:18 +0100

    Serializing notification response

--- src/Service/PostNotificationService.php
+++ src/Service/PostNotificationService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 
 use App\Entity\Post;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Component\Mercure\Update;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -18,15 +19,20 @@ class PostNotificationService
 
     private $mercureConfig;
 
+    /** @var SerializerInterface */
+    private $serializer;
+
     /**
      * PostNotificationService constructor.
      * @param MessageBusInterface $bus
+     * @param SerializerInterface $serializer
      * @param $mercureConfig
      */
-    public function __construct(MessageBusInterface $bus, $mercureConfig)
+    public function __construct(MessageBusInterface $bus, SerializerInterface $serializer, $mercureConfig)
     {
         $this->messageBus = $bus;
         $this->mercureConfig = $mercureConfig;
+        $this->serializer = $serializer;
     }
 
     /**
@@ -34,7 +40,9 @@ class PostNotificationService
      */
     public function sendUpdateNotification(Post $post) {
         $data = [
-            'message' => "The post '{$post->getTitle()}' has been updated"
+            'message' => "The post '{$post->getTitle()}' has been updated",
+            'target' => 'post',
+            'payload' => $post
         ];
         $this->notifyChannel($post, $data);
 
@@ -46,7 +54,9 @@ class PostNotificationService
      */
     public function sendDeleteNotification(Post $post) {
         $data = [
-            'message' => "The post '{$post->getTitle()}' has been deleted"
+            'message' => "The post '{$post->getTitle()}' has been deleted",
+            'target' => 'post',
+            'payload' => $post
         ];
         $this->notifyChannel($post, $data);
 
@@ -58,8 +68,10 @@ class PostNotificationService
      */
     protected function notifyChannel(Post $post, $data)
     {
+        $jsonContent = $this->serializer->serialize($data, 'json');
+
         $target = [$this->mercureConfig['target_base_url'] . $this->mercureConfig['posts_target_path']. '/' .$post->getId()];
-        $message = new Update($this->mercureConfig['target_base_url']. $this->mercureConfig['notification_channel_path'] , json_encode($data), $target);
+        $message = new Update($this->mercureConfig['target_base_url']. $this->mercureConfig['notification_channel_path'] , $jsonContent, $target);
 
         $this->messageBus->dispatch($message);
     }
